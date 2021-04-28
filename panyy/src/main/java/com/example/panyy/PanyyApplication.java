ae35707335235b7cf1b821c48c431c4bd7890750
panyy <panyy@shterm.com>
2021-04-28 16:08:56 +0800

    panyy

---
+++ panyy/src/main/java/com/example/panyy/PanyyApplication.java
@@ -0,0 +1,13 @@
+package com.example.panyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanyyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanyyApplication.class, args);
+	}
+
+}
---
+++ panyy/src/test/java/com/example/panyy/PanyyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.panyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanyyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
